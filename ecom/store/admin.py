9439847ff08b926e03a53c0138712a29adb93b99
Alok Babu <alokbabuyadhuvanshi@gmail.com>
2024-10-31 07:42:21 +0530

    registered it on admin page

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -5,3 +5,22 @@ admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+# Mix profile info and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# Extend the user model 
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username","first_name","last_name","email"]
+    inlines = [ProfileInline] 
+
+# Unregister the old way
+
+admin.site.unregister(User)
+
+# Re-register the new way
+
+admin.site.register(User, UserAdmin)
