536d5b3b82f5226bd4e3344720c634e42c111d25
Alok Babu <alokbabuyadhuvanshi@gmail.com>
2024-11-06 22:57:13 +0530

    update personalised cart

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,9 +1,10 @@
-from store.models import Product
+from store.models import Product,Profile
 
 class Cart():
     def __init__(self,request):
         self.session = request.session
-
+        #Get request
+        self.request = request
         # Get the current session key if it exists
         cart = self.session.get('session_key')
 
@@ -26,6 +27,17 @@ class Cart():
             self.cart[product_id] = product_Qty
         
         self.session.modified = True
+
+        # Deal with logged in users
+        if self.request.user.is_authenticated:
+            # Get the current user profile
+            current_user = Profile.objects.filter(user__id=self.request.user)
+            # convert {'3':1,'2':4} to {"3":1, "2":4}
+            carty = str(self.cart)
+            carty = carty.replace("\'", "\"")
+            # save carty to the profile model
+            current_user.update(old_cart = str(carty))
+
 
     def cart_total(self):
         # Get products ID'S
