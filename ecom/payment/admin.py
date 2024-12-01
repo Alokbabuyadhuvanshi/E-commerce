f76753367e4250124118eca18f8afc3e1d73b843
Alok Babu <alokbabuyadhuvanshi@gmail.com>
2024-12-01 19:37:29 +0530

    register it on admin page

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
 # Register your models on the admin section.
 
+admin.site.register(Order)
+admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+
