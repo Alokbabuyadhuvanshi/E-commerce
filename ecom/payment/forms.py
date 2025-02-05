dab81c2b2ca9d92623eaec141567fa09d71088eb
Alok Babu <alokbabuyadhuvanshi@gmail.com>
2025-02-05 12:21:59 +0530

    add payment form to handle billing information in checkout process

--- ecom/payment/forms.py
+++ ecom/payment/forms.py
@@ -80,3 +80,86 @@ class ShippingForm(forms.ModelForm):
         fields = ['shipping_full_name', 'shipping_email', 'shipping_address1', 'shipping_address2', 'shipping_city', 'shipping_state', 'shipping_zipcode', 'shipping_country']
 
         exclude = ['user',]
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '🧑 Name on Card',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=True
+    )
+    card_number = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '💳 Card Number',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=True
+    )
+    card_exp_date = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '📅 Expiration Date (MM/YY)',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=True
+    )
+    card_cvv_number = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '🔒 CVV',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=True
+    )
+    card_address1 = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '🏠 Billing Address1',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=True
+    )
+    card_address2 = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '🏠 Billing Address2',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=False
+    )
+    card_city = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '🏙️ City',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=True
+    )
+    card_zipcode = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '📬 Zipcode',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=True
+    )
+    card_country = forms.CharField(
+        label="", 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3 rounded-pill',
+            'placeholder': '🌍 Country',
+            'style': 'padding-left: 30px;',
+        }), 
+        required=True
+    )
--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -58,3 +58,4 @@ class OrderItem(models.Model):
     def __str__(self):
         return f'Order Item - {str(self.id)}'
 
+4
