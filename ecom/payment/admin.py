a706aa902809c1a63cca71f717b269789d0a39b7
Alok Babu <alokbabuyadhuvanshi@gmail.com>
2025-02-08 08:28:53 +0530

    extend admin interface for Order model with inline OrderItem and add shipped field to model

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 # Register your models on the admin section.
 
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+
+
+# Create an orderItem Inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+# Extend Our Order Model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields =['date_ordered']
+    fields = ["user","full_name","email","Shipping_Address","amount_paid","date_ordered","shipped"]
+    inlines = [OrderItemInline]
+
+# Unregister the Order Model
+
+admin.site.unregister(Order)
+
+# Re-Register our Order And OrderAdmin
+admin.site.register(Order, OrderAdmin)
 
--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -40,7 +40,8 @@ class Order(models.Model):
     Shipping_Address = models.TextField(max_length=15000)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    
+    shipped = models.BooleanField(default=False)
+
     def __str__(self):
         return f'Order - {str(self.id)}'
 
